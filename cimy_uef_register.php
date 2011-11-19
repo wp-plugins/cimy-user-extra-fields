4e3cada2ce0704fab66a10213e9c860df20fd877
Cimmo <Cimmo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-19 13:15:53 +0000

    Fix security issue with recaptcha that can be avoided
    git-svn-id: https://plugins.svn.wordpress.org/cimy-user-extra-fields/trunk@465058 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cimy_uef_register.php
+++ cimy_uef_register.php
@@ -545,7 +545,7 @@ function cimy_registration_check($user_login, $user_email, $errors) {
 		}
 	}
 
-	if ((isset($_POST["recaptcha_response_field"])) && ($options['captcha'] == "recaptcha") && (!empty($options['recaptcha_private_key']))) {
+	if (($options['captcha'] == "recaptcha") && (!empty($options['recaptcha_private_key']))) {
 		$recaptcha_code_ok = false;
 
 		if ($_POST["recaptcha_response_field"]) {

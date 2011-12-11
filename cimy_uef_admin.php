f5c333fef813f8506f1a79626f6e7e3327de78ae
Cimmo <Cimmo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-11 13:15:07 +0000

    Remove obsolete code
    git-svn-id: https://plugins.svn.wordpress.org/cimy-user-extra-fields/trunk@473446 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cimy_uef_admin.php
+++ cimy_uef_admin.php
@@ -1129,7 +1129,6 @@ function cimy_admin_users_list_page() {
 	if (is_network_admin()) {
 		require_once(ABSPATH . 'wp-admin/includes/class-wp-ms-users-list-table.php');
 		class WP_Cimy_Users_List_Table extends WP_MS_Users_List_Table {
-			var $old_args = array();
 			function prepare_items() {
 				global $role, $usersearch, $wpdb;
 
@@ -1232,7 +1231,6 @@ function cimy_admin_users_list_page() {
 	else {
 		require_once(ABSPATH . 'wp-admin/includes/class-wp-users-list-table.php');
 		class WP_Cimy_Users_List_Table extends WP_Users_List_Table {
-			var $old_args = array();
 			function prepare_items() {
 				global $role, $usersearch;
 

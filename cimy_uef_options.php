367474eb9c8e337ef5a07fe12dc9745ca2144e6e
Cimmo <Cimmo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-10 00:50:12 +0000

    Do not register all fieldset options string to wpml, only section name.
    git-svn-id: https://plugins.svn.wordpress.org/cimy-user-extra-fields/trunk@665765 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cimy_uef_options.php
+++ cimy_uef_options.php
@@ -35,7 +35,7 @@ function cimy_save_options() {
 	$fieldset_titles = explode(",", $options['fieldset_title']);
 	if (!empty($fieldset_titles)) {
 		foreach ($fieldset_titles as $fset_key => $fieldset) {
-			cimy_wpml_register_string("a_opt_fieldset_title_".$fset_key, $options['fieldset_title']);
+			cimy_wpml_register_string("a_opt_fieldset_title_".$fset_key, $fieldset);
 		}
 	}
 
